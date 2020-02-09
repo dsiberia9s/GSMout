5a324620b458be4908af11869a64114b9741301a
Dimi <34534835+dsiberia9s@users.noreply.github.com>
2020-02-09 18:03:05 +0700

    maybe ok..

--- GSMout.ino
+++ GSMout.ino
@@ -1,32 +1,38 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WebServer.h>
+#include "SPIFFS.h"
+#include "FS.h"
 
 #define RX_PIN  16
 #define TX_PIN  17
 #define RESET_PIN 5   
 
+WiFiUDP udp;
+NTPClient ntp(udp, 7 * 3600);
+WebServer web(80);
+
+unsigned long p = 0;
+
 char * s2c(String s) {
-  char * t = new char[s.length() + 1];
-  strcpy(t, s.c_str());
+  char * t_ = new char[s.length() + 1];
+  strcpy(t_, s.c_str());
+  char * t = t_;
+  delete t_;
   return t;
 }
 
 String parseString(int idSeparator, char separator, String str) { 
   String output = "";
   int separatorCout = 0;
-  for (int i = 0; i < str.length(); i++)
-  {
-    if ((char)str[i] == separator)
-    {
+  for (int i = 0; i < str.length(); i++) {
+    if ((char)str[i] == separator) {
       separatorCout++;
-    }
-    else
-    {
-      if (separatorCout == idSeparator)
-      {
+    } else {
+      if (separatorCout == idSeparator) {
         output += (char)str[i];
-      }
-      else if (separatorCout > idSeparator)
-      {
+      } else if (separatorCout > idSeparator) {
         break;
       }
     }
@@ -38,6 +44,56 @@ void debug(String s) {
   M5.Lcd.fillScreen(0);
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.print(s);
+}
+
+char * WiFiAuto(int timeout = 5000) {
+  timeout = (timeout < 1000) ? 1000 : timeout;
+  File file;
+  char * WiFiSSID;
+  char * WiFiPswd;
+  const char * path;
+  bool r = false;
+  WiFi.disconnect(true);
+  delay(100);
+  for (int i = 0; i < WiFi.scanNetworks(); i++) {
+    File root = SPIFFS.open("/");
+    file = root.openNextFile();
+    while (file) {
+      if (!file.isDirectory()) {
+        String WiFiSSID_ = WiFi.SSID(i);
+        WiFiSSID = new char[WiFiSSID_.length() + 1];
+        strcpy(WiFiSSID, WiFiSSID_.c_str());
+        path = file.name();
+        if ((strstr(path, WiFiSSID) != NULL) && strstr(file.name(), ".wifi")) {
+          r = true;
+        }
+        if (r) break;
+      }
+      file = root.openNextFile();
+    }
+    root.close();
+    if (r) break;
+  }
+  if (r) {
+    file = SPIFFS.open(path);
+    String WiFiPswd_ = "";
+    while (file.available()){
+      char ch = file.read();
+      if (ch == 0x0A) break;
+      WiFiPswd_ += ch;
+    }
+    file.close();
+    WiFiPswd = new char[WiFiPswd_.length() + 1];
+    strcpy(WiFiPswd, WiFiPswd_.c_str());
+    WiFi.begin((char *)WiFiSSID, (char *)WiFiPswd);
+    delete WiFiPswd;
+    int s = 250;
+    for (int i = 0; i < timeout; i += s) {
+      if (WiFi.status() == WL_CONNECTED) return WiFiSSID;
+      delay(s);
+    }
+  }
+  return NULL;
 }
 
 String AT(String s, unsigned long timeout = 10000) {
@@ -66,6 +122,7 @@ String AT(String s, unsigned long timeout = 10000) {
 bool modemBegin() {
   Serial2.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);  
   pinMode(RESET_PIN, OUTPUT);
+  //return true;
   
   digitalWrite(RESET_PIN, LOW);
   delay(1000);
@@ -85,6 +142,43 @@ bool modemBegin() {
   return false;
 }
 
+void reg(String number, String message = "") {
+  File file = SPIFFS.open(((message == "") ? "/calls.txt" : "/sms.txt"), FILE_APPEND);
+  file.print(ntp.getEpochTime());
+  file.print('\t');
+  file.print(ntp.getFormattedTime());
+  file.print('\t');
+  if (message == "") {
+    file.print(number);
+  } else {
+    file.print(number);
+    file.print('\t');
+    file.print(message);
+  }
+  file.print(0x1F);
+  file.close();
+}
+
+String getReg(String path) {
+  File file = SPIFFS.open(path.c_str());
+  String t = "<script>var data = \"";
+  if (file) {
+    while (file.available()) {
+      t += (char)file.read();
+    }
+  }
+  t += "\";</script>";
+  file.close();
+  file = SPIFFS.open("/index.html");
+  if (file) {
+    while (file.available()) {
+      t += (char)file.read();
+    }
+  }
+  file.close();
+  return t;
+}
+
 void modemLive() {
   String b = "";
   while (Serial2.available()) {
@@ -93,26 +187,60 @@ void modemLive() {
   if (strstr(s2c(b), "+CLIP:")) {
     String number = parseString(1, '\"', parseString(3, '\r', b));
     AT("ATH0\r"); // повесить трубку
-    M5.Lcd.print(number);
-  } 
-  if (strstr(s2c(b), "+CMT:")) {
+    reg(number);
+    //M5.Lcd.print(number);
+  } else if (strstr(s2c(b), "+CMT:")) {
     String number = parseString(1, '\"', parseString(1, '\r', b));
     String message = parseString(1, '\n', parseString(1, '\r', parseString(6, '\"', b)));
-    M5.Lcd.print(number);
-    M5.Lcd.print(message);
+    reg(number, message);
+    //M5.Lcd.print(number);
+    //M5.Lcd.print(message);
   }
 }
 
 void setup() {
   M5.begin();
-  if( modemBegin() ) 
-    debug("modem ok");
+
+  SPIFFS.begin(true);
+
+  M5.Lcd.println("Searching Wi-Fi, please wait...");
+  char * ssid = WiFiAuto(); 
+  if (ssid) {
+    M5.Lcd.println("Connectid to ");
+    M5.Lcd.println(ssid);
+    M5.Lcd.println(WiFi.localIP());
+  } else {
+    M5.Lcd.println("Can't connect to WiFi network :(\nTry to REBOOT device");
+  }
+
+  M5.Lcd.println("Starting NTP, please wait...");
+  ntp.begin();
+
+  M5.Lcd.println("Starting WEB, please wait...");
+  web.on("/sms", []() {
+    web.send(200, "text/html", getReg("/sms.txt")); 
+  });
+  web.on("/calls", []() {
+    web.send(200, "text/html", getReg("/calls.txt"));
+  });
+  web.onNotFound([]() {
+    web.send(404, "text/html", "404");
+  });
+  web.begin();
+
+  M5.Lcd.println("Starting modem, please wait...");
+  if ( modemBegin() ) 
+    M5.Lcd.println("Modem OK");
   else
-    debug("modem err");
-    
+    M5.Lcd.println("Modem FAIL");
 }
 
 void loop() {
+  if (millis() - p >= 1000) {
+    ntp.update();
+    p = millis();
+  }
+  web.handleClient();
   modemLive();
-  delay(100);
+  delay(10);
 }
